624b52f39e4c313429723ad8d3714adc414e6234
victor-onu <victoronushaibu@gmail.com>
2020-07-23 22:20:17 +0100

    deploy react

---
+++ src/main/resources/static/precache-manifest.03f64cacd81ae1d771827c5ed0d84212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5dd1f68e5da60aa41fb7947075ce570",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4804cb40e714fae25cbc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6e0e2d8b874ff0002579",
+    "url": "/static/js/2.bb688565.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bb688565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4804cb40e714fae25cbc",
+    "url": "/static/js/main.bf07eefc.chunk.js"
+  },
+  {
+    "revision": "fe2af3bd6869259f6b81",
+    "url": "/static/js/runtime-main.49925162.js"
+  }
+]);
